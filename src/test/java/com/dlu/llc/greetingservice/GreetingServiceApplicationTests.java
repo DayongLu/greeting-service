bbec79a24c5c02c0f37ec3707c763dc7021f4c62
Dayong lu <ludayong20@gmail.com>
2018-01-25 15:42:44 -0500

    change url

--- src/test/java/com/dlu/llc/greetingservice/GreetingServiceApplicationTests.java
+++ src/test/java/com/dlu/llc/greetingservice/GreetingServiceApplicationTests.java
@@ -2,15 +2,28 @@ package com.dlu.llc.greetingservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingServiceApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<Map> body = this.restTemplate.getForEntity("/greeting/{name}", Map.class, (Object) "Dayong");
+		String value = (String) body.getBody().get("greeting");
+		assertThat(value).contains("Dayong");
 	}
 
 }
